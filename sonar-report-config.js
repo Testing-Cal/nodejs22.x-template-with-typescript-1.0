074b12684d248944894951a75728918b86048e92
Ganesh Garle <ggarle@CALT00038.local>
2024-08-13 17:11:21 +0530

    Added missing file

---
+++ sonar-report-config.js
@@ -0,0 +1,5 @@
+// sonar-report-config.js
+module.exports = {
+  outputDirectory: "./coverage", // Directory where the report will be saved
+  outputName: "sonar-report.xml", // Name of the report file
+};
